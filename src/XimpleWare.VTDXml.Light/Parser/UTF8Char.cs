82410e1237f34c1f3506e7f5d807669f43a81219
M. David Peterson <m.david@3rdandurban.com>
2012-01-23 01:31:51 -0800

    Reformatting and line ending transforms. Removal of duplicates after copying, moving and renaming files to match the naming convention of the overall solution.

--- src/XimpleWare.VTDXml.Light/Parser/UTF8Char.cs
+++ src/XimpleWare.VTDXml.Light/Parser/UTF8Char.cs
@@ -15,52 +15,51 @@
 * along with this program; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
-using System;
 namespace com.ximpleware.parser
 {
-	/// <summary> UTF 8 char range finder.
-	/// Creation date: (3/3/04 6:01:49 PM)
-	/// </summary>
-	/// <author> : 
-	/// </author>
-	public class UTF8Char
-	{
-		//UPGRADE_NOTE: Final was removed from the declaration of 'charLen'. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1003'"
-		internal static readonly sbyte[] charLen = new sbyte[]{1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1, 
-            1, 1, 1, 1, 1, 1, 1, 1,
-            -1,-1,-1,-1,-1,-1,-1,-1,
-            -1,-1,-1,-1,-1,-1,-1,-1, 
-            - 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
-            - 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
-            - 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
-            - 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
-            - 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
-            - 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
-            2, 2, 2, 2, 2, 2, 2, 2, 
-            2, 2, 2, 2, 2, 2, 2, 2, 
-            2, 2, 2, 2, 2, 2, 2, 2, 
-            2, 2, 2, 2, 2, 2, 2, 2, 
-            3, 3, 3, 3, 3, 3, 3, 3, 
-            3, 3, 3, 3, 3, 3, 3, 3, 
-            4, 4, 4, 4, 4, 4, 4, 4, 
-            5, 5, 5, 5, 6, 6, - 1, - 1};
-		static public int byteCount(int n)
-		{
-			return charLen[n];
-		}
-	}
+    /// <summary> UTF 8 char range finder.
+    /// Creation date: (3/3/04 6:01:49 PM)
+    /// </summary>
+    /// <author> : 
+    /// </author>
+    public class UTF8Char
+    {
+        //UPGRADE_NOTE: Final was removed from the declaration of 'charLen'. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1003'"
+        internal static readonly sbyte[] charLen = new sbyte[]{1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1, 
+						1, 1, 1, 1, 1, 1, 1, 1,
+						-1,-1,-1,-1,-1,-1,-1,-1,
+						-1,-1,-1,-1,-1,-1,-1,-1, 
+						- 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
+						- 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
+						- 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
+						- 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
+						- 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
+						- 1, - 1, - 1, - 1, - 1, - 1, - 1, - 1, 
+						2, 2, 2, 2, 2, 2, 2, 2, 
+						2, 2, 2, 2, 2, 2, 2, 2, 
+						2, 2, 2, 2, 2, 2, 2, 2, 
+						2, 2, 2, 2, 2, 2, 2, 2, 
+						3, 3, 3, 3, 3, 3, 3, 3, 
+						3, 3, 3, 3, 3, 3, 3, 3, 
+						4, 4, 4, 4, 4, 4, 4, 4, 
+						5, 5, 5, 5, 6, 6, - 1, - 1};
+        static public int byteCount(int n)
+        {
+            return charLen[n];
+        }
+    }
 }
